1e391c249ae1018e2967cb7d482e26eec0e943d2
Everett <everett.bowline@gmail.com>
2018-05-04 00:19:08 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,3 +9,19 @@ def key_for_min_value(name_hash)
   end
   low_key
 end
+
+def key_for_min_value(name_hash)
+  smallest_key = ""
+  smallest_value = 1000000000000000
+  if name_hash == {}
+    return nil
+  else 
+    name_hash.each do |key, value|
+      if value < smallest_value
+        smallest_value = value
+        smallest_key = key
+      end
+    end
+    smallest_key
+  end
+end
